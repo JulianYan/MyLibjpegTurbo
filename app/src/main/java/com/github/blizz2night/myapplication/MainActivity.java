44302fafb914205531d3c4cf10dcdc81e6c148a8
JulianYan <julian.yan@nothing.tech>
2022-09-08 20:32:46 +0800

    update jpegturbo

--- app/src/main/java/com/github/blizz2night/myapplication/MainActivity.java
+++ app/src/main/java/com/github/blizz2night/myapplication/MainActivity.java
@@ -3,14 +3,22 @@ package com.github.blizz2night.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.libjpegturbo.turbojpeg.TJ;
+import org.libjpegturbo.turbojpeg.TJCompressor;
+import org.libjpegturbo.turbojpeg.TJException;
+import org.libjpegturbo.turbojpeg.YUVImage;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        TJCompressor tjc = null;
+
+        try (InputStream inputStream = getAssets().open("I420_6144x8192.yuv")){
+            int available = inputStream.available();
+            Log.i(TAG, "run: available=" + available);
+            byte[] yuvBuf = new byte[available];
+            inputStream.read(yuvBuf);
+            tjc = new TJCompressor();
+            tjc.setJPEGQuality(95);
+            int width = 6144;
+            int height = 8192;
+            YUVImage yuvImage = new YUVImage(yuvBuf, width, 1, height, TJ.SAMP_420);
+            tjc.setSourceImage(yuvImage);
+            int bufSize = TJ.bufSize(width, height, TJ.SAMP_420);
+            Log.i(TAG, "run: bufSize=" + bufSize);
+            byte[] dstBuf = new byte[bufSize];
+            Log.i(TAG, "compress: +++");
+            tjc.compress(dstBuf, 0);
+            Log.i(TAG, "compress: ---");
+            tjc.close();
+            File cacheDir = getCacheDir();
+            File file = new File(cacheDir, "test.jpeg");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(dstBuf);
+            fileOutputStream.close();
+            /* Write the JPEG image to disk. */
+        } catch (IOException e) {
+            Log.e(TAG, "run: ", e);
+        }
+
+    }
 }
